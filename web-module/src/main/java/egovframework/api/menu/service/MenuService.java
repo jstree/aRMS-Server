fd04a6cb36af6045d3b2ffa4f8a7761da052a7d8
LeeDongMin <admin@313.co.kr>
2021-10-17 13:13:26 +0900

    [SmartCommit] SP-581 #comment aRMS 코드 삭제
    [ Java Service Tree Framework(JSTF)::DevPortal ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/jstree
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- web-module/src/main/java/egovframework/api/menu/service/MenuService.java
+++
@@ -1,7 +0,0 @@
-package egovframework.api.menu.service;
-
-import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
-
-public interface MenuService extends JsTreeHibernateService{
-
-}
--- web-module/src/main/java/egovframework/api/menu/service/MenuServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package egovframework.api.menu.service;
-
-import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateServiceImpl;
-import org.springframework.stereotype.Service;
-
-@Service("MenuService")
-public class MenuServiceImpl extends JsTreeHibernateServiceImpl implements MenuService {
-
-}
